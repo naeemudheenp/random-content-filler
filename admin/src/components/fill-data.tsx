b56ed8e85f89259cfbc87e71d5052c72e4539eac
naeemudheen p <nayeemudheenaslam3@gmail.com>
2024-12-10 23:26:26 +0530

    Added support for test area

--- admin/src/components/fill-data.tsx
+++ admin/src/components/fill-data.tsx
@@ -13,21 +13,20 @@ export function FillRandomData() {
         try {
           const randomData = generateRandomData();
 
-          const inputFields = document.querySelectorAll('input, textarea, select');
+          const inputFields = document.querySelectorAll(
+            'input, textarea, select,div[role="textbox"]'
+          );
 
           inputFields.forEach((input: any) => {
-            if (input.type === 'text' || input.tagName.toLowerCase() === 'textarea') {
-              if (input.name === 'title') {
-                input.value = randomData.title;
-              } else if (input.name === 'description') {
-                input.value = randomData.description;
-              } else {
-                input.value = `Random ${Math.random().toString(36).substring(7)}`;
-              }
-            } else if (input.tagName.toLowerCase() === 'select') {
-              const options = input.options;
-              const randomIndex = Math.floor(Math.random() * options.length);
-              input.selectedIndex = randomIndex;
+            if (input.name === 'title') {
+              input.value = randomData.title;
+              console.log('DIVDFAFDS', input);
+            } else if (input.name === 'description') {
+              input.value = randomData.description;
+            } else if (input.tagName === 'DIV') {
+              input.textContent = randomData.description;
+            } else {
+              input.value = `Random ${Math.random().toString(36).substring(7)}`;
             }
           });
         } catch (error) {
